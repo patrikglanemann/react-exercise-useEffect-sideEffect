e60eed92bf55002245dbd1645160bc560730d90f
PatrikG <Patrik0Gn@gmail.com>
2021-07-19 14:50:42 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("Hello React");
   // Your code below
+
+  useEffect(() => {
+    setMessage("Hello neuefische");
+  });
 
   return (
     <div className="App">
