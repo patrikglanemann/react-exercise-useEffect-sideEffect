e95bdd3a49240d296222343bcaa3866d7d1db4fe
PatrikG <Patrik0Gn@gmail.com>
2021-07-19 15:45:36 +0200

    Add helper function

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import "./App.css";
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // Your code below
+  // return must be the same function, so it can not be an anonymouse, otherwise we can't reference to it
 
   useEffect(() => {
-    window.addEventListener("resize", () => {
+    function helper() {
       setWindowWidth(window.innerWidth);
-    });
+    }
+
+    window.addEventListener("resize", helper);
     return () => {
-      window.removeEventListener("resize", () => {
-        setWindowWidth(window.innerHeight);
-      });
+      window.removeEventListener("resize", helper);
     };
   }, []);
 
